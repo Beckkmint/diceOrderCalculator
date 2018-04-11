4fad82adf6818311e71da4f934178f75cf6e2d8c
Beckkmint <562962148@qq.com>
2018-04-11 04:19:43 +0100

    modify

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,15 @@ public class Main {
     public static void main(String [ ] args){
         ProcessOrder processOrder = new ProcessOrder();
         Scanner scanner = new Scanner(System.in);
+        String s;
         System.out.println("Please input transformation order:");
-        String s = scanner.next();
-        if (s.length() > 50)
-            System.out.println("Maximum input length is 50!");
-        else
-            processOrder.processLoop(s);
+        if(scanner.hasNext()) {
+            s = scanner.nextLine();
+            if (s.length() > 50)
+                System.out.println("Maximum input length is 50!");
+            else
+                processOrder.processLoop(s);
+        }
+        scanner.close();
     }
 }
